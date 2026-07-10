063a1825306412136993d85b11735d948c0ce36a
neuro-filip <filip@neuromorphyx.com>
2026-07-10 18:20:10 +0200

    Remove stray MKR fast inference project

--- mkr_akd1500_infer_fast/program_info_blob.h
+++
@@ -1,9 +0,0 @@
-#ifndef AKIDA_PROGRAM_INFO_BLOB_H_
-#define AKIDA_PROGRAM_INFO_BLOB_H_
-
-#include <cstdint>
-
-extern const unsigned char program_info_blob[];
-extern const int64_t program_info_blob_len;
-
-#endif  // AKIDA_PROGRAM_INFO_BLOB_H_
